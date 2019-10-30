996de045d4cdd7a1a2b23ce27690c27a0f151429
Jamie <phillipsja97@gmail.com>
2019-10-30 12:36:18 -0500

    fixed login/logout issues

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,16 @@
 import firebase from 'firebase';
 import apiKeys from './helpers/apikeys.json';
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import nav from './components/myNavBar/myNavBar';
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  console.error(apiKeys.firebaseKeys);
+  auth.logInButton();
+  authData.checkLoginStatus();
+  nav.logoutEvent();
 };
 
 init();
