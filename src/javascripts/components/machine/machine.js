14f30cfbb9a682b79246a3fc9ea49936fa012626
Jamie <phillipsja97@gmail.com>
2019-11-02 12:30:19 -0500

    snackPositions added

--- src/javascripts/components/machine/machine.js
+++ src/javascripts/components/machine/machine.js
@@ -1,13 +1,6 @@
 import smash from '../../helpers/data/smash';
 import './machine.scss';
 
-// 1. getMachines - returns first machine (hard coding)
-// 2. use MachineId to get all positions for that machine
-// 3. use MachineId to get all snack positions
-// 4. use uId of snackPositions/positions to get snack for that machine
-// 5. smash them - return an array of positions (in order a1, a2, ....) so positions should
-// have position and snack if a snack exists at that position.
-
 const buildTheMachine = () => {
   smash.getCompleteMachine()
     .then((singleMachine) => console.log('1 machine', singleMachine))
--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,10 +1,21 @@
 import machineData from './machineData';
 import positionData from './positionData';
+import snackPositionData from './snackPosition';
+
+// 1. getMachines - returns first machine (hard coding)
+// 2. use MachineId to get all positions for that machine - DONE
+// 3. use MachineId to get all snack positions - DONE
+// 4. use uId of snackPositions/positions to get snack for that machine
+// 5. smash them - return an array of positions (in order a1, a2, ....) so positions should
+// have position and snack if a snack exists at that position.
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
   machineData.getMachine()
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
-    .then((positions) => resolve(positions))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => resolve(snackPositions));
+    })
     .catch((error) => reject(error));
 });
 
