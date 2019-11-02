9153f11b1f2ea3347169a661fe8be51b74b70436
Jamie <phillipsja97@gmail.com>
2019-11-02 13:10:17 -0500

    data complete, next Smash

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,11 +1,12 @@
 import machineData from './machineData';
 import positionData from './positionData';
 import snackPositionData from './snackPosition';
+import snacksData from './snackData';
 
-// 1. getMachines - returns first machine (hard coding)
+// 1. getMachines - returns first machine (hard coding) - DONE
 // 2. use MachineId to get all positions for that machine - DONE
 // 3. use MachineId to get all snack positions - DONE
-// 4. use uId of snackPositions/positions to get snack for that machine
+// 4. use uId of snackPositions/positions to get snack for that machine - DONE
 // 5. smash them - return an array of positions (in order a1, a2, ....) so positions should
 // have position and snack if a snack exists at that position.
 
@@ -14,7 +15,12 @@ const getCompleteMachine = () => new Promise((resolve, reject) => {
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
     .then((positions) => {
       snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
-        .then((snackPositions) => resolve(snackPositions));
+        .then((snackPositions) => {
+          snacksData.getSnacksByUid(positions[0].uId).then((snacks) => {
+            console.log('snackPositions', snackPositions);
+            resolve(snacks);
+          });
+        });
     })
     .catch((error) => reject(error));
 });
